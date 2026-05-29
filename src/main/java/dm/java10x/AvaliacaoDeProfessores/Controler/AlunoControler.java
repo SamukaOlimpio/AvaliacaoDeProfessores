4f58a9fc1c4ace5d5d0176f828a2d1030ba4e142
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-05-29 12:13:16 -0300

    ajuste dos controllers

--- src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/AlunoControler.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/Controler/AlunoControler.java
@@ -1,10 +1,11 @@
 
 package dm.java10x.AvaliacaoDeProfessores.Controler;
 
+
+
 import dm.java10x.AvaliacaoDeProfessores.model.AlunoModel;
 import dm.java10x.AvaliacaoDeProfessores.service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
