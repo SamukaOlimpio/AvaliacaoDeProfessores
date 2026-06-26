6a77fcfa92df9ea949695ec8f9916c5099cdc9b1
Nescalll <joao.vitor.2007.10.08@gmail.com>
2026-06-26 17:37:23 -0300

    melhoria no DTO

--- src/main/java/dm/java10x/AvaliacaoDeProfessores/dto/AvaliacaoDaAulaDTO.java
+++ src/main/java/dm/java10x/AvaliacaoDeProfessores/dto/AvaliacaoDaAulaDTO.java
@@ -2,5 +2,5 @@ package dm.java10x.AvaliacaoDeProfessores.dto;
 
 import dm.java10x.AvaliacaoDeProfessores.enumeradores.Adjetivo;
 
-public record AvaliacaoDaAulaDTO(Long id_aluno, Long id_professor, Adjetivo  adjetivo, Integer nota) {
+public record AvaliacaoDaAulaDTO(String email, Long id_professor, Adjetivo  adjetivo, Integer nota) {
 }
